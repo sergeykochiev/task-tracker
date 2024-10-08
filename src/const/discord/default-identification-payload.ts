868eb98e8b82b358db81f2bb17f9976acade37d5
sergeykochiev <sergeyivanovo547@gmail.com>
2024-10-08 17:35:00 +0300

    added initial presence update to default indentification payload

--- src/const/discord/default-identification-payload.ts
+++ src/const/discord/default-identification-payload.ts
@@ -1,9 +1,10 @@
-import { GatewayIntentBits, GatewayOpcodes } from "discord-api-types/v10"
+import { GatewayIdentify, GatewayIntentBits, GatewayOpcodes } from "discord-api-types/v10"
 import DiscordConfig from "../../config/env/discord.config"
 import os from "node:os"
 import DiscordConst from "./discord"
+import INITIAL_PRESENCE_UPDATE_PAYLOAD from "./initial-presence-update-payload"
 
-const DEFAULT_IDENTIFY_PAYLOAD = {
+const DEFAULT_IDENTIFY_PAYLOAD: GatewayIdentify = {
     op: GatewayOpcodes.Identify,
     d: {
         token: DiscordConfig.DISCORD_TOKEN,
@@ -12,6 +13,7 @@ const DEFAULT_IDENTIFY_PAYLOAD = {
             browser: DiscordConst.AUTH_HEADERS["User-Agent"],
             device: DiscordConst.AUTH_HEADERS["User-Agent"]
         },
+        presence: INITIAL_PRESENCE_UPDATE_PAYLOAD,
         intents: GatewayIntentBits.GuildMessages + GatewayIntentBits.DirectMessages + GatewayIntentBits.MessageContent
     }
 } as const
