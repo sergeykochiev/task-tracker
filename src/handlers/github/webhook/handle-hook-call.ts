64c2dd3574d7c523b2bddeb56bf7a4ffdd165931
sergeykochiev <sergeyivanovo547@gmail.com>
2024-10-08 13:43:21 +0300

    added github hook handler template

---
+++ src/handlers/github/webhook/handle-hook-call.ts
@@ -0,0 +1,5 @@
+import { Request, Response } from "express";
+
+export default async function handleGithubHookCall(req: Request, res: Response) {
+    return
+}
--- src/utils/github/init-webhook-api.ts
+++ src/utils/github/init-webhook-api.ts
@@ -1,9 +1,10 @@
 import express from 'express';
+import handleGithubHookCall from '../../handlers/github/webhook/handle-hook-call';
 
 export default function initGithubHookApi() {
     const app = express()
     const githubWebhookRouter = express.Router()
-    githubWebhookRouter.post("/")
+    githubWebhookRouter.post("/:repoSlug", handleGithubHookCall)
     app.use('/github', githubWebhookRouter)
     return app
 }
