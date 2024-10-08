ab5246850df7a8c9e17d66bb38c4750e81b7ff40
sergeykochiev <sergeyivanovo547@gmail.com>
2024-10-08 17:33:30 +0300

    added default presence

---
+++ src/const/discord/initial-presence-update-payload.ts
@@ -0,0 +1,10 @@
+import { PresenceUpdateStatus, GatewayPresenceUpdateData } from "discord-api-types/v10";
+
+const INITIAL_PRESENCE_UPDATE_PAYLOAD: GatewayPresenceUpdateData = {
+    since: (new Date()).getTime(),
+    activities: [],
+    status: PresenceUpdateStatus.Online,
+    afk: false
+} as const
+
+export default INITIAL_PRESENCE_UPDATE_PAYLOAD
