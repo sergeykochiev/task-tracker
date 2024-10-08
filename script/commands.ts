f9ecf59209232820f9f9529cd5ea3a1aef9ed7e7
sergeykochiev <sergeyivanovo547@gmail.com>
2024-10-09 00:31:59 +0300

    fixed commands

--- script/commands.ts
+++ script/commands.ts
@@ -1,13 +1,12 @@
 import 'dotenv/config';
 import envconfig from '../src/config/env/discord.config.js';
 import InstallGlobalCommands from '../src/utils/discord/install-global-commands.js';
-import CommandTypes from '../src/enum/discord/command-types.js';
-import { ApplicationIntegrationType, InteractionContextType } from "discord-api-types/v10"
+import { ApplicationCommandType, ApplicationIntegrationType, InteractionContextType } from "discord-api-types/v10"
 
 const REGISTER_COMMAND = {
     name: 'register',
     description: 'Registers a chat as a task supplier',
-    type: CommandTypes.CHAT_INPUT,
+    type: ApplicationCommandType.ChatInput,
     integration_types: [ApplicationIntegrationType.GuildInstall],
     contexts: [InteractionContextType.Guild],
 } as const
@@ -15,7 +14,7 @@ const REGISTER_COMMAND = {
 const CONFIGURE_COMMAND = {
     name: 'configure',
     description: 'Configures roles and users that are able to register chats',
-    type: CommandTypes.CHAT_INPUT,
+    type: ApplicationCommandType.ChatInput,
     integration_types: [ApplicationIntegrationType.GuildInstall],
     contexts: [InteractionContextType.Guild],
 } as const
