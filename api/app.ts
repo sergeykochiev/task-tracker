8a94aa75538c9ee247c626b0d7104939c3680871
sergeykochiev <sergeyivanovo547@gmail.com>
2024-10-08 13:43:46 +0300

    installed db utilities and libs

--- api/app.ts
+++ api/app.ts
@@ -1,5 +1,6 @@
 import runDiscordWebSocket from "../src/utils/discord/run-websocket"
 import runGithubHookApi from "../src/utils/github/run-webhook-api"
+import "reflect-metadata"
 
 runDiscordWebSocket()
 module.exports = runGithubHookApi()
