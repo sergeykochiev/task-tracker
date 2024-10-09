910eac253d773e64535e6f5802978cd037c6690b
sergeykochiev <sergeyivanovo547@gmail.com>
2024-10-09 23:16:41 +0300

    added one more handler

---
+++ src/utils/github/event-handlers/handle-pull-request-review.ts
@@ -0,0 +1,5 @@
+import GithubPullRequestReviewWebhookEventPayload from "../../../types/github/webhook/payload/event/pull-request-review.event";
+
+export default async function handlePullRequestReview(data: GithubPullRequestReviewWebhookEventPayload) {
+    return
+}
