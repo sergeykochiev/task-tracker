266b6d726d1e68f909b7c5f429fc4d17c38ee71d
sergeykochiev <sergeyivanovo547@gmail.com>
2024-10-10 13:20:23 +0300

    shamelessly yoinked signature verification logic from github docs

--- src/utils/github/handle-webhook-call.ts
+++ src/utils/github/handle-webhook-call.ts
@@ -2,11 +2,13 @@ import { Response } from "express";
 import GithubWebhookRequest from "../../types/github/webhook/webhook-request";
 import handleGithubWebhookEvent from "./handle-webhook-event";
 import GithubWebhookEventHandlerArgs from "../../types/utils/handle-github-webhook-event-args";
+import validateWebhookSignature from "./validate-webhook-call";
 
 export default async function handleGithubWebhookCall(req: GithubWebhookRequest, res: Response) {
+    if (!(await validateWebhookSignature(req.body, req.headers["X-Hub-Signature-256"].split("=")[1]))) res.status(401).send("Unauthorized")
     handleGithubWebhookEvent({
         eventType: req.headers["X-GitHub-Event"],
-        data: req.body 
+        data: JSON.parse(req.body)
     } as GithubWebhookEventHandlerArgs)
     return
 }
---
+++ src/utils/github/validate-webhook-call.ts
@@ -0,0 +1,43 @@
+import GithubConfig from "../../config/env/github.config"
+
+function hexToBytes(hex: string) {
+    let len = hex.length / 2;
+    let bytes = new Uint8Array(len);
+
+    let index = 0;
+    for (let i = 0; i < hex.length; i += 2) {
+        let c = hex.slice(i, i + 2);
+        let b = parseInt(c, 16);
+        bytes[index] = b;
+        index += 1;
+    }
+
+    return bytes;
+}
+
+const encoder = new TextEncoder()
+
+export default async function validateWebhookSignature(body: string, signature: string): Promise<boolean> {
+    const secretBytes = encoder.encode(GithubConfig.WEBHOOK_SECRET)
+    const bodyBytes = encoder.encode(body)
+    const signatureBytes = hexToBytes(signature)
+    const cryptoAlgorithm = {
+        name: "HMAC",
+        hash: {
+            name: 'SHA-256'
+        }
+    }
+    const cryptoKey = await crypto.subtle.importKey(
+        "raw",
+        secretBytes,
+        cryptoAlgorithm,
+        false,
+        ["sign", "verify"]
+    )
+    return crypto.subtle.verify(
+        cryptoAlgorithm.name,
+        cryptoKey,
+        signatureBytes,
+        bodyBytes
+    )
+}
