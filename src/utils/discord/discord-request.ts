16a118ef61a61dc12d11d3efd9878c4c56426864
sergeykochiev <sergeyivanovo547@gmail.com>
2024-10-11 00:19:10 +0300

    simplified request function logic

--- src/utils/discord/discord-request.ts
+++ src/utils/discord/discord-request.ts
@@ -1,6 +1,17 @@
 import DiscordConst from "../../const/discord/discord";
-import requestFactory from "../request-factory";
+import DiscordApiRequestError from "../../error/discord/request-error";
 
-const discordMakeRequest = requestFactory(DiscordConst)
+async function discordMakeRequest(endpoint: string, options: Record<string, any>): Promise<Response> {
+    const url = DiscordConst.URL.API_ROOT + endpoint
+    if (options.body) options.body = JSON.stringify(options.body)
+    const res = await fetch(url, {
+        headers: DiscordConst.AUTH_HEADERS,
+        ...options
+    })
+    if (!res.ok) {
+        throw new DiscordApiRequestError(res.status, res.statusText)
+    }
+    return res;
+}
 
 export default discordMakeRequest
--- src/utils/github/github-request.ts
+++ src/utils/github/github-request.ts
@@ -1,6 +1,18 @@
 import GithubConst from "../../const/github/github";
-import requestFactory from "../request-factory";
 
-const githubMakeRequest = requestFactory(GithubConst)
+async function githubMakeRequest(endpoint: string, options: Record<string, any>): Promise<Response> {
+    const url = GithubConst.URL.API_ROOT + endpoint
+    if (options.body) options.body = JSON.stringify(options.body)
+    const res = await fetch(url, {
+        headers: GithubConst.AUTH_HEADERS,
+        ...options
+    })
+    if (!res.ok) {
+        const data = await res.json()
+        console.log(res.status)
+        throw new Error(JSON.stringify(data))
+    }
+    return res;
+}
 
 export default githubMakeRequest
