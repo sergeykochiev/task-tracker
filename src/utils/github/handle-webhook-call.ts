3a5aed570d586f9db616d7549f202bf50e7cab63
sergeykochiev <sergeyivanovo547@gmail.com>
2024-10-10 13:21:51 +0300

    improved code readability

--- src/utils/github/handle-webhook-call.ts
+++ src/utils/github/handle-webhook-call.ts
@@ -5,7 +5,9 @@ import GithubWebhookEventHandlerArgs from "../../types/utils/handle-github-webho
 import validateWebhookSignature from "./validate-webhook-call";
 
 export default async function handleGithubWebhookCall(req: GithubWebhookRequest, res: Response) {
-    if (!(await validateWebhookSignature(req.body, req.headers["X-Hub-Signature-256"].split("=")[1]))) res.status(401).send("Unauthorized")
+    const signature = req.headers["X-Hub-Signature-256"].split("=")[1]
+    const valid = await validateWebhookSignature(req.body, signature)
+    if (!valid) res.status(401).send("Unauthorized")
     handleGithubWebhookEvent({
         eventType: req.headers["X-GitHub-Event"],
         data: JSON.parse(req.body)
