f760d4d1e1f91fab4f245a33ff026f9ec53be866
sergeykochiev <sergeyivanovo547@gmail.com>
2024-10-10 12:51:36 +0300

    updated configs

--- src/config/env/discord.config.ts
+++ src/config/env/discord.config.ts
@@ -1,15 +1,15 @@
 import "dotenv/config"
 
-const { APP_ID, DISCORD_TOKEN, PUBLIC_KEY } = process.env
+const { DISCORD_APP_ID, DISCORD_TOKEN, DISCORD_PUBLIC_KEY } = process.env
 
-if (!APP_ID || !DISCORD_TOKEN || !PUBLIC_KEY) {
+if (!DISCORD_APP_ID || !DISCORD_TOKEN || !DISCORD_PUBLIC_KEY) {
     throw new Error("Not enough env data: discord")
 }
 
 const DiscordConfig = {
-    APP_ID: APP_ID,
-    DISCORD_TOKEN: DISCORD_TOKEN,
-    PUBLIC_KEY: PUBLIC_KEY
+    APP_ID: DISCORD_APP_ID,
+    TOKEN: DISCORD_TOKEN,
+    PUBLIC_KEY: DISCORD_PUBLIC_KEY
 } as const
 
 export default DiscordConfig
--- src/config/env/github.config.ts
+++ src/config/env/github.config.ts
@@ -1,13 +1,14 @@
 import "dotenv/config"
 
-const { ACCESS_TOKEN } = process.env
+const { GITHUB_ACCESS_TOKEN, GITHUB_WEBHOOK_KEY } = process.env
 
-if (!ACCESS_TOKEN) {
+if (!GITHUB_ACCESS_TOKEN || !GITHUB_WEBHOOK_KEY) {
     throw new Error("Not enough env data: github")
 }
 
 const GithubConfig = {
-    ACCESS_TOKEN: ACCESS_TOKEN,
+    ACCESS_TOKEN: GITHUB_ACCESS_TOKEN,
+    WEBHOOK_SECRET: GITHUB_WEBHOOK_KEY
 } as const
 
 export default GithubConfig
