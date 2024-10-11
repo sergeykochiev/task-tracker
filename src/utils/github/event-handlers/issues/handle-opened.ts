b2d6c08ecef577acf0e0f94367cb62b8112f6a7e
sergeykochiev <sergeyivanovo547@gmail.com>
2024-10-12 01:41:53 +0300

    returned issue opened handling logoc, untested

--- src/utils/github/event-handlers/issues/handle-opened.ts
+++ src/utils/github/event-handlers/issues/handle-opened.ts
@@ -1,33 +1,35 @@
 import { IssuesOpenedEvent } from "@octokit/webhooks-types";
 import AppDataSource from "../../../../db/data-source";
-import GithubIssueTrackEntity from "../../../../db/entity/gh-issue-track.entity";
-import discordSendMessageToChannel from "../../../discord/send-message";
+import GithubIssueTrackEntity from "../../../../db/entity/issue-track.entity";
+import discordSendMessageToChannel from "../../../discord/helpers/send-message";
 import TrackerEntity from "../../../../db/entity/tracker.entity";
 import { APIMessage } from "discord-api-types/v10"
 
 export default async function githubHandleIssuesOpenedEvent(data: IssuesOpenedEvent) {
-    // const issueRepository = AppDataSource.getRepository(GithubIssueTrackEntity)
-    // const trackerRepository = AppDataSource.getRepository(TrackerEntity)
-    // const targetTracker = await trackerRepository.findOne({
-    //     where: {
-    //         gh_repo: {
-    //             gh_repo_id: data.repository.id
-    //         }
-    //     }
-    // })
-    // if (!targetTracker) return
-    // const targetChannel = targetTracker.dc_channel
+    const issueRepository = AppDataSource.getRepository(GithubIssueTrackEntity)
+    const trackerRepository = AppDataSource.getRepository(TrackerEntity)
+    const targetTracker = await trackerRepository.findOne({
+        where: {
+            github_repository: {
+                owner: data.repository.owner.name,
+                name: data.repository.name
+            }
+        }
+    })
+    if (!targetTracker) return
     //incorrect type prbbly, here ↓
-    const targetMessage: APIMessage | null = await discordSendMessageToChannel("1292208485555245175", {
+    const targetMessage: APIMessage | null = await discordSendMessageToChannel(targetTracker.discord_channel_id, {
         content: `New issue was created`
     })
-    // if (!targetMessage) return
-    // const newIssue: GithubIssueTrackEntity = {
-    //     gh_issue_id: data.issue.id,
-    //     dc_original_message_id: targetMessage.id,
-    //     gh_repo: targetTracker.gh_repo,
-    //     dc_channel: targetChannel
-    // }
-    // await issueRepository.save(newIssue)
+    if (!targetMessage) return
+    const newIssue: GithubIssueTrackEntity = {
+        id: data.issue.id,
+        original_message: {
+            id: targetMessage.id,
+            tracker: targetTracker
+        },
+        github_repository: targetTracker.github_repository,
+    }
+    await issueRepository.save(newIssue)
     return
 }
