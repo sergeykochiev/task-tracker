0392d93435efd28b8f6a9d704f3efe3d0881629a
sergeykochiev <sergeyivanovo547@gmail.com>
2024-10-12 01:45:00 +0300

    switched to pg

--- src/db/data-source.ts
+++ src/db/data-source.ts
@@ -1,23 +1,25 @@
 import { DataSource } from "typeorm";
 import DatabaseConfig from "../config/env/db.config";
 import TrackerEntity from "./entity/tracker.entity";
-import GithubRepoEntity from "./entity/gh-repo.entity";
-import DiscordGuildEntity from "./entity/dc-guild.entity";
-import DiscordChannelEntity from "./entity/dc-channel.entity";
-import GithubIssueTrackEntity from "./entity/gh-issue-track.entity";
+import RepositoryEntity from "./entity/repository.entity";
+import OriginalMessageEntity from "./entity/original-message.entity";
+import IssueTrackEntity from "./entity/issue-track.entity";
+import GuildEntity from "./entity/guild.entity";
 
 const AppDataSource = new DataSource({
-    type: "sqlite",
-    // host: "localhost",
-    // port: 5432,
-    // username: "test",
-    // password: "test",
-    database: DatabaseConfig.DB_NAME,
+    type: "postgres",
+    host: DatabaseConfig.HOST,
+    port: DatabaseConfig.PORT,
+    username: DatabaseConfig.USERNAME,
+    password: DatabaseConfig.PASSWORD,
+    database: DatabaseConfig.NAME,
     synchronize: true,
-    logging: true,
-    entities: [TrackerEntity, GithubRepoEntity, DiscordGuildEntity, DiscordChannelEntity, GithubIssueTrackEntity],
+    logging: false,
+    entities: [TrackerEntity, RepositoryEntity, OriginalMessageEntity, IssueTrackEntity, GuildEntity],
     subscribers: [],
     migrations: [],
 })
+
+AppDataSource.initialize()
 
 export default AppDataSource
