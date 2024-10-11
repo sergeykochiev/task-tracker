8bc63417c8763557754ecfb706674f37205221db
sergeykochiev <sergeyivanovo547@gmail.com>
2024-10-12 01:46:51 +0300

    added env config for pg

--- src/config/env/db.config.ts
+++ src/config/env/db.config.ts
@@ -1,13 +1,17 @@
 import "dotenv/config"
 
-const { DB_NAME } = process.env
+const { DB_NAME, DB_HOST, DB_PORT, DB_USERNAME, DB_PASSWORD } = process.env
 
-if (!DB_NAME) {
+if (!DB_NAME || !DB_HOST || !DB_PORT || !DB_USERNAME || !DB_PASSWORD) {
     throw new Error("Not enough env data: db")
 }
 
 const DatabaseConfig = {
-    DB_NAME: DB_NAME,
+    NAME: DB_NAME,
+    HOST: DB_HOST,
+    PORT: +DB_PORT,
+    USERNAME: DB_USERNAME,
+    PASSWORD: DB_PASSWORD
 } as const
 
 export default DatabaseConfig
