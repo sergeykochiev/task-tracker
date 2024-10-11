239bef3372cfae077c856c0790ac889674daa632
sergeykochiev <sergeyivanovo547@gmail.com>
2024-10-12 01:45:45 +0300

    added new app env

--- src/config/env/app.config.ts
+++ src/config/env/app.config.ts
@@ -1,7 +1,12 @@
-const { PORT } = process.env
+const { APP_PORT, APP_HOST } = process.env
+
+if (!APP_HOST) {
+    throw new Error("Not enough env data: db")
+}
 
 const AppConfig = {
-    PORT: PORT || 3000,
+    PORT: APP_PORT || 3000,
+    HOST: APP_HOST
 } as const
 
 export default AppConfig
