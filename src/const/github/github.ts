c137d7cb5140c68ebfd4ebb8e77d542f17520be3
sergeykochiev <sergeyivanovo547@gmail.com>
2024-10-12 12:02:31 +0300

    adad

--- src/const/github/github.ts
+++ src/const/github/github.ts
@@ -5,12 +5,8 @@ const GithubConst = {
             HOOKS: (owner: string, repo: string) => `/repos/${owner}/${repo}/hooks`,
             LIST_REPOS: "/user/repos",
             GET_REPO_BY_ID: (repoId: string) => `/repositories/${repoId}`,
-<<<<<<< HEAD
             GET_REPO: (repoOwner: string, repoName: string) => `/repos/${repoOwner}/${repoName}`,
             EDIT_ORIGINAL_INTERACTION_RESPONSE: (appId: string, interactionToken: string) => `/webhooks/${appId}/${interactionToken}/messages/@original`
-=======
-            GET_REPO: (repoOwner: string, repoName: string) => `/repos/${repoOwner}/${repoName}`
->>>>>>> d4af1e04dd7ba2ca32181e9c8d8acd9b69838ce0
         }
     },
     AUTH_HEADERS: (accessToken: string) => {
