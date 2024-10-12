6162b5914e4d1b7b4a273ca9602cb514c55fef70
sergeykochiev <sergeyivanovo547@gmail.com>
2024-10-12 12:02:10 +0300

    kljb

--- src/utils/discord/open-and-init-websocket.ts
+++ src/utils/discord/open-and-init-websocket.ts
@@ -1,10 +1,5 @@
-<<<<<<< HEAD
 import getDiscordWebSocketUrl from "./api/get-websocket-url"
-=======
-import getDiscordWebSocketUrl from "./helpers/get-websocket-url"
->>>>>>> d4af1e04dd7ba2ca32181e9c8d8acd9b69838ce0
 import DiscordWebsocketConnection from "../../discord/websocket-connection"
-import discordHandleMessageCreate from "./event-handlers/handle-message-create"
 import discordHandleCommands from "./event-handlers/interaction/handle-application-command"
 import discordHandleModalSubmit from "./event-handlers/interaction/handle-modal-submit"
 import discordHandleButtonComponent from "./event-handlers/interaction/message-component/handle-button"
@@ -13,7 +8,7 @@ export default async function discordOpenAndInitWebSocket() {
     const wssUrl = await getDiscordWebSocketUrl()
     const discordWebsocketConnection = new DiscordWebsocketConnection(wssUrl)
     discordWebsocketConnection.onCommand = discordHandleCommands
-    discordWebsocketConnection.onMessageCreate = discordHandleMessageCreate
+    // discordWebsocketConnection.onMessageCreate = discordHandleMessageCreate
     // discordWebsocketConnection.onMessageComponent = discordHandleMessageComponent
     discordWebsocketConnection.onModalSubmit = discordHandleModalSubmit
     discordWebsocketConnection.onButtonComponent = discordHandleButtonComponent
