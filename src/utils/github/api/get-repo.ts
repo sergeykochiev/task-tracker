a23470dee12ccd452a9ca92d053576af10815a2d
sergeykochiev <sergeyivanovo547@gmail.com>
2024-10-16 01:25:41 +0300

    merge conflicts cleanup

--- src/utils/github/api/get-repo.ts
+++ src/utils/github/api/get-repo.ts
@@ -1,7 +1,6 @@
 import { Repository } from "@octokit/webhooks-types"
 import githubMakeRequest from "../github-request"
 import { GITHUB_ENDPOINTS } from "../../../const/api/github.api"
-import { GITHUB_ENDPOINTS } from "../../../const/api/github.api"
 
 export default async function githubGetRepository(repoOwner: string, repoName: string, token?: string): Promise<Repository> {
     return await (await githubMakeRequest(GITHUB_ENDPOINTS.GET_REPO(repoOwner, repoName), token)).json()
--- src/utils/github/github-request.ts
+++ src/utils/github/github-request.ts
@@ -2,13 +2,11 @@ import GithubAppConfig from "../../config/env/github.config";
 import { GITHUB_API_ROOT, GITHUB_HEADERS } from "../../const/api/github.api";
 import ApiRequestError from "../../error/app/request.error";
 import githubGetAuthHeaders from "./auth/get-auth-headers";
-import githubGetAuthHeaders from "./auth/get-auth-headers";
 
 async function githubMakeRequest(endpoint: string, token: string = GithubAppConfig.CLIENT_SECRET, options?: Record<string, any>): Promise<Response> {
     const url = GITHUB_API_ROOT + endpoint
     if (options && options.body) options.body = JSON.stringify(options.body)
     const res = await fetch(url, {
-        headers: token ? githubGetAuthHeaders(token) : GITHUB_HEADERS,
         headers: token ? githubGetAuthHeaders(token) : GITHUB_HEADERS,
         ...options
     })
