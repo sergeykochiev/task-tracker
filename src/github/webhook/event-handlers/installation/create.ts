584954cdd80e87df357263a0b9dcf9d2155a8b70
sergeykochiev <sergeyivanovo547@gmail.com>
2024-11-01 01:10:16 +0300

    changed because roles are now handled differently

--- src/github/webhook/event-handlers/installation/create.ts
+++ src/github/webhook/event-handlers/installation/create.ts
@@ -1,15 +1,11 @@
 import { InstallationCreatedEvent, Organization } from "@octokit/webhooks-types";
-import InstallationEntity from "../../../../db/entity/installation.entity";
-import discordSendMessageToChannel from "../../../../discord/api/messages/send-message";
-import { DISCORD_PING_ROLE_SELECT } from "../../../../const/discord/default";
 import RegisterStatus from "../../../../db/enum/register-status";
 import TrackerEntity from "../../../../db/entity/tracker.entity";
-import handleChannelRegistrationFailure from "../../../../utils/general/handle-channel-registration-failure";
 import { makeDatabaseRequest } from "../../../../utils/db/repository-request";
-import { log } from "console";
 import RepositoryEntity from "../../../../db/entity/repository.entity";
+import discordSendMessageToChannel from "../../../../utils/discord/api/messages/send-message";
 
-async function updateEachRepository(owner: string, name: string, newInstallation: InstallationEntity) {
+async function updateEachRepository(owner: string, name: string, installationId: number) {
     const getRepositoryRes = await makeDatabaseRequest(RepositoryEntity, "findOneBy", {
         owner: owner,
         name: name
@@ -17,7 +13,7 @@ async function updateEachRepository(owner: string, name: string, newInstallation
     if (getRepositoryRes.err || !getRepositoryRes.data) return
     const targetRepositoryId = getRepositoryRes.data.id
     await makeDatabaseRequest(RepositoryEntity, "update", targetRepositoryId, {
-        installation: newInstallation 
+        installationId: String(installationId) 
     })
     const getTrackersRes = await makeDatabaseRequest(TrackerEntity, "findBy", {
         github_repository: {
@@ -26,59 +22,27 @@ async function updateEachRepository(owner: string, name: string, newInstallation
         register_status: RegisterStatus.PendingInstallation
     })
     if (getTrackersRes.err !== null) return
-    getTrackersRes.data.map(async (tracker: TrackerEntity) => await gotoRoleSelectRegisterationPhase(tracker.discord_channel_id))
-}
-
-export async function gotoRoleSelectRegisterationPhase(trackerId: TrackerEntity["discord_channel_id"]) {
-    const getTrackerRes = await makeDatabaseRequest(TrackerEntity, "findOneById", trackerId)
-    if (getTrackerRes.err || !getTrackerRes.data) {
-        log(getTrackerRes.err)
-        return
-    }
-    if (!getTrackerRes.data.role_to_ping) {
-        const updateTrackerRes = await makeDatabaseRequest(TrackerEntity, "update", trackerId, {
-            register_status: RegisterStatus.PendingRole
+    getTrackersRes.data.map(async (tracker: TrackerEntity) => {
+        const updateTrackerRes = await makeDatabaseRequest(TrackerEntity, "update", tracker.discord_channel_id, {
+            register_status: RegisterStatus.Registered
         })
-        if (updateTrackerRes.err) {
-            handleChannelRegistrationFailure(trackerId, "Database request error")
+        if(updateTrackerRes.err !== null) {
+            await discordSendMessageToChannel(tracker.discord_channel_id, {
+                content: "Installation was detected, but an error in updating the channel status accured."
+            })
             return
         }
-        const messageRes = await discordSendMessageToChannel(trackerId, {
-            content: "Github App successfully installed. Now select the role you want to ping on events:",
-            components: [DISCORD_PING_ROLE_SELECT]
+        await discordSendMessageToChannel(tracker.discord_channel_id, {
+            content: "Installation detected, registration successful."
         })
-        if (messageRes.err) {
-            handleChannelRegistrationFailure(trackerId, "Discord API request error")
-        }
-        return
-    }
-    const updateTrackerRes = await makeDatabaseRequest(TrackerEntity, "update", trackerId, {
-        register_status: RegisterStatus.Registered
     })
-    if (updateTrackerRes.err) {
-        handleChannelRegistrationFailure(trackerId, "Database request error")
-        return
-    }
-    await discordSendMessageToChannel(trackerId, {
-        content: "Channel is registered."
-    })
-    return
 }
 
 export default async function githubHandleInstallationCreateEvent(data: InstallationCreatedEvent & {
     organization?: Organization
 }) {
     if (!data.repositories) return
-    const saveInstallationRes = await makeDatabaseRequest(InstallationEntity, "save", {
-        id: String(data.installation.id),
-        user_id: String(data.sender.id),
-        organization_id: data.organization && String(data.organization.id)
-    })
-    if (saveInstallationRes.err !== null) {
-        log(saveInstallationRes.err)
-        return
-    }
     const owner = data.organization ? data.organization.login : data.sender.login
-    data.repositories.map(async (repository) => await updateEachRepository(owner, repository.name, saveInstallationRes.data))
+    data.repositories.map(async (repository) => await updateEachRepository(owner, repository.name, data.installation.id))
     return
 }
